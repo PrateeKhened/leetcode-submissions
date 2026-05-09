deda3c016e00a740074abea2e8bf31c97595d54d
Prateek Khened <code.prateekkhened@gmail.com>
2026-05-10 00:40:28 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = defaultdict(list)
+        for u, v in prerequisites:
+            d[u].append(v)
+        
+        UNVISITED = 0 
+        VISITING = 1 
+        VISITED = 2 
+
+        states = [UNVISITED] * numCourses
+    
+        def dfs(node):
+            state = states[node]
+
+            if state == VISITED: return True 
+            elif state == VISITING: return False 
+
+            states[node] = VISITING
+
+            for nei in d[node]:
+                if not dfs(nei):
+                    return False 
+
+            states[node] = VISITED
+
+            return True 
+             
+        
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False 
+            
+        
+        return True 
