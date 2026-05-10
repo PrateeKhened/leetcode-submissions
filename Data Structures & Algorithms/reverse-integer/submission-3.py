3a33d524ee3bcc7ea0a586c245204cd41ea93619
Prateek Khened <code.prateekkhened@gmail.com>
2026-05-11 01:10:49 +0530

    Add: reverse-integer - submission-3

---
+++ Data Structures & Algorithms/reverse-integer/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        res = 0 
+        while x:
+            digit = int(math.fmod(x, 10))
+            x = int(x / 10)
+
+            if (res < -214748364 or (res == -214748364 and digit < -8)):
+                return 0 
+            
+            if (res > 214748364 or (res == 214748364 and digit > 7)):
+                return 0 
+            
+            res = (res * 10) + digit 
+        
+        return res
