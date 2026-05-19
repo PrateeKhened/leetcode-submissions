8423bfe0bef5ec822b365e8c862d39ef7b235bc9
Prateek Khened <code.prateekkhened@gmail.com>
2026-05-20 01:04:24 +0530

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res, sol = [], [] 
+
+        def dfs(i):
+            if i == len(s):
+                res.append(sol[:])
+                return 
+            
+            for j in range(i, len(s)):
+                sub = s[i : j + 1]
+
+                if sub[::-1] == sub:
+                    sol.append(sub)
+                    dfs(j + 1)
+                    sol.pop() 
+        
+        dfs(0)
+        return res
