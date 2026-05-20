a936726e842636114c1e8d8c884c3e382e9bea78
Prateek Khened <code.prateekkhened@gmail.com>
2026-05-21 00:45:48 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,34 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = {}
+
+    def addWord(self, word: str) -> None:
+        node = self.trie
+        for c in word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node["*"] = True
+
+    def search(self, word: str) -> bool:
+        def dfs(i, node):
+            if i == len(word):
+                return "*" in node
+
+            c = word[i]
+
+            if c == ".":
+                for k, v in node.items():
+                    if k == "*":
+                        continue
+                    if dfs(i + 1, v):
+                        return True
+                return False
+
+            if c not in node:
+                return False
+
+            return dfs(i + 1, node[c])
+
+        return dfs(0, self.trie)
